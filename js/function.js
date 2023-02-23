37b98f277779f8a0930c55ea62ed771a54653123
Marianela Rojas <marianroj.2342@gmail.com>
2023-02-23 09:33:33 -0300

    Cuarta escritura en el repositorio

--- js/function.js
+++ js/function.js
@@ -1,11 +1,17 @@
-/**
-function cambioColor() {
-    target.style.color = '#6600FF' ;
+
+
+/** 
+document.getElementById('menusobremi').onmouseover = function () {
+    document.getElementById('menusobremi').style.color = '#6600FF' ;
 }
-
-document.getElementById('barramenu').onmouseover = cambioColor();
-*/
-
+/**
 document.getElementById("boton_color").addEventListener("click", function () {
     document.body.style.backgroundColor = "#FF0000";
 })
+
+*/
+
+
+document.getElementById('botonmail').onclick = function () {
+    document.getElementById('mail').style.display='block' ;
+}
