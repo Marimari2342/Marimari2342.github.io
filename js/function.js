b4b192bcfc4f872583091d0b546d79109c5da15b
Marianela Rojas <marianroj.2342@gmail.com>
2023-02-23 02:24:01 -0300

    Tercer escritura en el repositorio

---
+++ js/function.js
@@ -0,0 +1,11 @@
+/**
+function cambioColor() {
+    target.style.color = '#6600FF' ;
+}
+
+document.getElementById('barramenu').onmouseover = cambioColor();
+*/
+
+document.getElementById("boton_color").addEventListener("click", function () {
+    document.body.style.backgroundColor = "#FF0000";
+})
