fc7292765073465cee5d1b3088dd2ce4e023556c
Marianela Rojas <marianroj.2342@gmail.com>
2023-02-25 00:25:36 -0300

    Octava escritura en el repositorio

--- js/function.js
+++ js/function.js
@@ -1,11 +1,65 @@
 /** Botones menu */
 document.getElementById('menu1').onmouseover = function () {
-    document.getElementById('menu1').style.color ="#e6e6e6"
+    document.getElementById('menu1').style.color ="#e6e6e6" ;
 }
-
+document.getElementById('menu1').onmouseleave = function () {
+    document.getElementById('menu1').style.color ="black" ;
+}
+document.getElementById('menu2').onmouseover = function () {
+    document.getElementById('menu2').style.color ="#e6e6e6" ;
+}
+document.getElementById('menu2').onmouseleave = function () {
+    document.getElementById('menu2').style.color ="black" ;
+}
+document.getElementById('menu3').onmouseover = function () {
+    document.getElementById('menu3').style.color ="#e6e6e6" ;
+}
+document.getElementById('menu3').onmouseleave = function () {
+    document.getElementById('menu3').style.color ="black" ;
+}
+document.getElementById('menu4').onmouseover = function () {
+    document.getElementById('menu4').style.color ="#e6e6e6" ;
+}
+document.getElementById('menu4').onmouseleave = function () {
+    document.getElementById('menu4').style.color ="black" ;
+}
+document.getElementById('menu5').onmouseover = function () {
+    document.getElementById('menu5').style.color ="#e6e6e6" ;
+}
+document.getElementById('menu5').onmouseleave = function () {
+    document.getElementById('menu5').style.color ="black" ;
+}
 /** Iconos */
-
-
+document.getElementById('mailicono').onmouseover = function () {
+    document.getElementById('maillista').style.fontWeight= "bold" ;
+} 
+document.getElementById('mailicono').onmouseleave = function () {
+    document.getElementById('maillista').style.fontWeight= "normal" ;
+} 
+document.getElementById('telicono').onmouseover = function () {
+    document.getElementById('tellista').style.fontWeight= "bold" ;
+} 
+document.getElementById('telicono').onmouseleave = function () {
+    document.getElementById('tellista').style.fontWeight= "normal" ;
+} 
+document.getElementById('diricono').onmouseover = function () {
+    document.getElementById('dirlista').style.fontWeight= "bold" ;
+} 
+document.getElementById('diricono').onmouseleave = function () {
+    document.getElementById('dirlista').style.fontWeight= "normal" ;
+} 
+document.getElementById('webicono').onmouseover = function () {
+    document.getElementById('weblista').style.fontWeight= "bold" ;
+} 
+document.getElementById('webicono').onmouseleave = function () {
+    document.getElementById('weblista').style.fontWeight= "normal" ;
+} 
+document.getElementById('linicono').onmouseover = function () {
+    document.getElementById('linlista').style.fontWeight= "bold" ;
+} 
+document.getElementById('linicono').onmouseleave = function () {
+    document.getElementById('linlista').style.fontWeight= "normal" ;
+} 
 
 /** Nombre formulario */
 document.getElementById('minombre').onmousedown = function () {
