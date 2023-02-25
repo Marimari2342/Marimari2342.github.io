efd406c7cc4975dcf4f23c7766e0f820be423735
Marianela Rojas <marianroj.2342@gmail.com>
2023-02-24 22:59:15 -0300

    Septima escritura en el repositorio

--- js/function.js
+++ js/function.js
@@ -1,15 +1,43 @@
-
+/** Botones menu */
 document.getElementById('menu1').onmouseover = function () {
     document.getElementById('menu1').style.color ="#e6e6e6"
 }
 
-document.getElementById('miconsulta').onclick = function () {
+/** Iconos */
+
+
+
+/** Nombre formulario */
+document.getElementById('minombre').onmousedown = function () {
+    document.getElementById('minombre').style.backgroundColor= "#e6e6e6" ;
+    document.getElementById('nombretitulo').style.fontWeight= "bold" ;
+}
+document.getElementById('minombre').onmouseup = function () {
+    document.getElementById('minombre').style.backgroundColor= "white" ;
+    document.getElementById('nombretitulo').style.fontWeight= "normal" ;
+}
+
+/** Mail formulario */
+document.getElementById('mimail').onmousedown = function () {
+    document.getElementById('mimail').style.backgroundColor= "#e6e6e6" ;
+    document.getElementById('mailtitulo').style.fontWeight= "bold" ;
+}
+document.getElementById('mimail').onmouseup = function () {
+    document.getElementById('mimail').style.backgroundColor= "white" ;
+    document.getElementById('mailtitulo').style.fontWeight= "normal" ;
+}
+
+/** Consulta formulario */
+document.getElementById('miconsulta').onmousedown = function () {
     document.getElementById('miconsulta').style.backgroundColor= "#e6e6e6" ;
     document.getElementById('consultatitulo').style.fontWeight= "bold" ;
 }
+document.getElementById('miconsulta').onmouseup = function () {
+    document.getElementById('miconsulta').style.backgroundColor= "white" ;
+    document.getElementById('consultatitulo').style.fontWeight= "normal" ;
+}
 
-
-
+/** Botón enviar formulario */
 document.getElementById('enviarform').onclick = function () {
     document.getElementById('enviarform').style.backgroundColor= "#66CC99" ;
 }
